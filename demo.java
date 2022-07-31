99328efb60e9c52bb9d6c64e2375d22113592ee6
IamViet <nguyenvietbocap2k3@gmail.com>
2022-07-31 17:34:45 +0700

    demogit

---
+++ demo.java
@@ -0,0 +1,13 @@
+class demo{
+
+    import java.io.*;
+
+    public static void readFile(){
+        
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        
+    }
+}
